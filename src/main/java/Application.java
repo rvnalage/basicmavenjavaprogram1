e977244c82ed3b16d2e3ed65ac4c17ec66edc088
rahul.nalage@gmail.com <Rhulnalage11@>
2021-02-10 23:34:14 +0530

    updated project 19

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String args[]) {
 
-        System.out.println("Hello Java github18");
+        System.out.println("Hello Java github19");
         Calculator cal = new Calculator();
         int m = cal.multiply(10,2);
         System.out.println(m);
